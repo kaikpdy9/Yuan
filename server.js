36ac147908c585237c9b52a201ecaacea57146df
kaikpdy9 <kaikpdy9@gmail.com>
2022-03-10 16:20:07 +0800

    upd: update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import userRoutes from "./backend/routes/userRoutes.js";
 dotenv.config()
 connectDB()
 const app =express()
+app.use(express.json())
 
 app.use('/api/users',userRoutes)
 app.listen(5000)
