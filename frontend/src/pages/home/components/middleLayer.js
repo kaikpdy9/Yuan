991226237a5dca9e330995d36598c4f68cdca413
kaikpdy9 <18315151342@163.com>
2022-03-26 10:19:53 +0800

    upd

--- frontend/src/pages/home/components/middleLayer.js
+++ frontend/src/pages/home/components/middleLayer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Mountain from "./mountain";
 
 const MiddleLayer=()=>{
     return(
         <div className="mt-20">
-            Middle Layer
+            <div>
+                <div className="grid-cols-12 grid">
+                    <div className="col-span-4 text-center">
+                    </div>
+                    <div className="col-span-4 text-center">
+                        <h3>scroll down</h3>
+                    </div>
+                    <div className="col-span-4 text-center">
+                        </div>
+                </div>
+
+            </div>
+            <div>
+                <Mountain/>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -9,7 +9,7 @@ const HomePage=()=>{
         <div className="mx-auto pt-40">
                     <div className="outsideOne h-banner w-full">
                             <p className="text-banner Megrim text-center leading-banner">Whisper words of</p>
-                        <div className="flex justify-center"><p className="text-9xl text-center leading-banner">Wisdom</p> <span className="text-xl leading-6 ml-9"> <p className="mt-10">I wish everything always</p><p>seems to work in</p><p>your favor</p></span>
+                        <div className="flex justify-center"><p className="text-9xl text-center leading-banner">Wisdom</p> <span className="text-xl leading-6 ml-9"> <p className="mt-10">chanson danse</p><p>seems to work in</p><p>your favor</p></span>
                             </div>
                             <p className="text-banner Megrim leading-banner text-center ml-40">Let it <span className="Ceviche leading-banner text-9xl"> be</span></p>
                     </div>
